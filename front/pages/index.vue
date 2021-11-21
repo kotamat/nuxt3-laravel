29507e6ff17796604bb837c0d7b1d1517f8f5a70
kotamat <kota1861@gmail.com>
2021-11-21 22:06:18 +0900

    fix refresh index page and typo

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -3,10 +3,7 @@
         <li v-for="item in data.data" :key="item.id">
             <nuxt-link :to="`/job/${item.id}`">{{ item.name }}</nuxt-link>
         </li>
-        <nuxt-link
-            :to="`/?page=${data.current_page - 1}`"
-            v-if="data.current_page > 0"
-        >past</nuxt-link>
+        <nuxt-link :to="`/?page=${data.current_page - 1}`" v-if="data.current_page > 0">prev</nuxt-link>
         <nuxt-link
             :to="`/?page=${data.current_page + 1}`"
             v-if="data.current_page < data.last_page"
@@ -16,9 +13,10 @@
 
 <script lang="ts" setup>
 const route = useRoute()
-const page = route.query.page || 1
-const { data,refresh } = await useFetch(`/api/job/get/?page=${page}`)
-watch(() => route.query, () => {
-    refresh(true) 
-    })
+const page = route.query.page || 0
+const { data } = await useFetch(`/api/job/get/?page=${page}`)
+watch(() => route.query, async () => {
+    const page = route.query.page || 0
+    data.value = await $fetch(`/api/job/get/?page=${page}`)
+})
 </script>
